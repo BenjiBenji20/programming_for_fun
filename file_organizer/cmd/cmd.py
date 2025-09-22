2626df7aaf74d10edbdfa76ca8c166d58b72d089
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-09-22 21:56:54 +0800

    -separate

---
+++ file_organizer/cmd/cmd.py
@@ -0,0 +1,10 @@
+
+# available commands as of the starting creation 09/21/2025.
+CMD = {
+    "goin", # <- nav to a parameter. Similar to cd
+    "org", # <- organize known files in a paramter
+    "undo", # <- only if existing command has been done, it undo the previous file org
+    'redo', # <- redo what has been undone
+    "exit" # <- exit the applicaiton 
+}
+
